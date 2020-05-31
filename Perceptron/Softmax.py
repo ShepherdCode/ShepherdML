d6ffd849433f8a7749f12228c34829b3cfa997fe
JasonRafeMiller <jmill02@shepherd.edu>
2020-05-31 10:07:52 -0400

    Create sample data.

--- Perceptron/Softmax.py
+++ Perceptron/Softmax.py
@@ -5,7 +5,7 @@ Implemented as multiclass shallow NN.
 Ouputs are probabilities 0 to 1.
 One output per class r; these sum to 1.
 Output is negative log-likelihood based on posterior probability.
-Loss function is cross-entropy: neg log of true class.
+Loss function is cross-entropy: neg log prob of true class given Xi.
 Udate is Wr=Wr+alpha{r is correct ? Xi * (1-P(r|Xi)) : -Xi * P(r|Xi)}.
 One hidden layer, one weight vector Wr per node, linear activation.
 One output node per r with softmax activation.
@@ -42,9 +42,21 @@ def say(statement):
     except Exception:
         pass
 
+def create_sample_data(filename):
+    with open (filename,"w") as csvfile:
+        writer = csv.writer(csvfile,delimiter=',')
+        writer.writerow(["n","x1","x2","x3","x4","x5","class"])
+        writer.writerow([1,1.3,1.0,1.2,1.1,1.4,"one"])
+        writer.writerow([1,1.3,1.0,1.2,1.1,1.4,"one"])
+        writer.writerow([2,2.3,2.0,2.2,2.1,2.4,"two"])
+        writer.writerow([2,2.3,2.0,2.2,2.1,2.4,"two"])
+        writer.writerow([3,3.3,3.0,3.2,3.1,3.4,"three"])
+        writer.writerow([3,3.3,3.0,3.2,3.1,3.4,"three"])
+
 def args_parse():
     global args
     parser = argparse.ArgumentParser(description='Linear classifier.')
+    parser.add_argument('--sample', help = 'Data file to create', type=str)
     parser.add_argument('--train', help = 'Labeled set filename', type=str)
     parser.add_argument('--classify', help = 'Unlabeled set filename', type=str)
     parser.add_argument('--debug', action = 'store_true')
@@ -58,6 +70,8 @@ if __name__ == '__main__':
     try:
         args_parse()
         nn = Multinomial_Logistic_Regression (3,1,5,3)
+        if args.sample is not None:
+            create_sample_data(args.sample)
         if args.train is not None:
             nn.train(args.train)
         if args.classify is not None:
