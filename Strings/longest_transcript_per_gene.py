6a5d79bdef8e28380e755714e4d28d74a6e0091b
JasonRafeMiller <Jason.Rafe.Miller@gmail.com>
2020-06-24 12:50:54 -0400

    Longest_transcript_per_gene reads and writes FASTA.

--- Strings/longest_transcript_per_gene.py
+++ Strings/longest_transcript_per_gene.py
@@ -1,25 +1,43 @@
 import sys
 
-saved={}
+'''
+Input FASTA on STDIN.
+Assume input defline formatted like this:
+>geneID otherstuff...
+Output FASTA on STDOUT.
+Output contains only the longest sequence per geneID.
+
+This is designed for parsing ensemble lncRNA fasta with deflines like
+>ENSG-ID ENST-ID otherstuff
+
+Usage:
+# cat infasta | longest_transcript_per_gene.py > outfasta
+'''
+
+deflines={}
+sequences={}
 lengths={}
+gene=""
 for rawline in sys.stdin:
     line=rawline.rstrip('\n')
-    fields = line.split('\t')
-    gene=fields[0]
-    seq=fields[3]
-    length=len(seq)
-    if gene not in lengths or lengths[gene] < length:
-        saved[gene]=line
-        lengths[gene]=length
+    if line[0]=='>':
+        defline=line
+        fields=line.split(' ')
+        gene=fields[0]
+    else:
+        sequence=line
+        length=len(line)
+        if gene not in lengths or lengths[gene] < length:
+            deflines[gene]=defline
+            sequences[gene]=sequence
+            lengths[gene]=length
+        gene=""
 
-for line in saved.values():
-    fields = line.split('\t')
-    gene=fields[0][0:15]
-    transcript=fields[1]
-    biotype=fields[2]
-    seq=fields[3]
-    print (">"+gene+" "+transcript+" "+biotype)
-    print (seq)
+for gene in deflines.keys():
+    defline = deflines[gene]
+    sequence = sequences[gene]
+    print(defline)
+    print(sequence)
 
 
 
