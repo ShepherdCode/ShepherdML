78547fb7a83caa131f584f66c0a722531f1d61dc
JasonRafeMiller <jmill02@shepherd.edu>
2020-07-14 13:29:41 -0400

    Move shell script to project directory.

--- Strings/subset_of_LncAtlas.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-# Extract a subset from LncAtlas.
-# + non-coding (nc)
-# + one cell line (HeLa.S3)
-# + one score type (cyto vs nuclear RCI)
-
-cat LncAtlas.download_all_raw_data.csv | grep "nc,nc" | grep "HeLa.S3,CNRCI" | grep -v "CNRCI,NA," | tr ',' '\t' > non-coding.Hela.CNRCI.tsv
