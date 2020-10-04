95c11763ffe6d8fb3bf5af1101714810c590ff0b
JasonRafeMiller <jmill02@shepherd.edu>
2020-10-04 17:32:27 -0400

    Reduction has infinite loop.

--- Strings/reduction.py
+++ Strings/reduction.py
@@ -11,33 +11,59 @@ class Reducer ():
         self.test_size = 500
         self.challenge_size = 500
         self.valid_size = 500
-        self.train_size = 500
-        self.all_genes=[]
+        self.train_size = 16000
+        self.all_genes={}
+        self.transcript_to_gene={}
+        self.set_aside={}
+        self.train_set_genes={}
 
     def data_in(self,infile):
-        self.all_genes=[]
+        TOKEN_SEPARATOR='|'
         with open(infile, 'r') as infa:
             num_seqs = 0
             for line in infa:
                 if line[0]==self.DEFLINE_PREFIX:
-                    transcript_id=line[1:18]
-                    gene_id=line[19:36]
-                    self.all_genes.append(gene_id)
-                else:
-                    seq=line
-        print(len(self.all_genes))
-        print(self.all_genes[0])
-        print(self.all_genes[1])
-        print(self.all_genes[len(self.all_genes)-1])
+                    words=line.split(TOKEN_SEPARATOR)
+                    transcript_id=words[0]
+                    gene_id=words[1]
+                    self.all_genes[gene_id]=1
+                    self.transcript_to_gene[transcript_id]=gene_id
 
-    def reduce():
-        pass
+    def reduce(self):
+        test_set=[]
+        for i in range(self.test_size):
+            gene_id=self.get_random_not_taken()
+            test_set.append(gene_id)
+        print("Size of test set is %d"%len(test_set))
+        challenge_set=[]
+        for i in range(self.challenge_size):
+            gene_id=self.get_random_not_taken()
+            challenge_set.append(gene_id)
+        print("Size of challenge set is %d"%len(challenge_set))
+        valid_set=[]
+        for i in range(self.valid_size):
+            gene_id=self.get_random_not_taken()
+            valid_set.append(gene_id)
+        print("Size of valid set is %d"%len(valid_set))
+        train_set=[]
+        for i in range(self.train_size):
+            gene_id=self.get_random_not_taken()
+            train_set.append(gene_id)
+        print("Size of train set is %d"%len(train_set))
+        self.train_set_genes = train_set
+
+    def get_random_not_taken(self):
+        list_genes=list(self.all_genes.keys())
+        num_genes = len(list_genes)
+        while (True):
+            rnd=random.randrange(0,num_genes)
+            gene_id=list_genes[rnd]
+            if gene_id not in self.set_aside:
+                self.set_aside[gene_id] = 1
+                return gene_id
 
     def data_out(self,infile):
         #with open(self.outfile, 'w') as outfa:
-        pass
-
-    def reduce(self):
         pass
 
     def print_prev(self,outfile,defline,seq):
