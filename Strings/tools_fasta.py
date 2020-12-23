f326a052d28f9e7066fa4e1f6aa616590a0b44d0
JasonRafeMiller <jmill02@shepherd.edu>
2020-12-23 10:47:08 -0500

    Add make_kmer_table() to tools_fasta.

--- Strings/tools_fasta.py
+++ Strings/tools_fasta.py
@@ -1,8 +1,8 @@
 '''
 Eventually, make this a proper python module.
 For now, just do this:
-import kmer_tools
-kmer_tools.yahoo()
+import tools_fasta as tools
+tools.yahoo()
 '''
 
 import pandas as pd
@@ -37,3 +37,23 @@ def load_fasta(filename,label):
     df4=pd.DataFrame(lens,columns=['seqlen'])
     df=pd.concat((df1,df2,df3,df4),axis=1)
     return df
+
+def make_kmer_table(K):
+    npad='N'*K
+    shorter_kmers=['']
+    for i in range(K):
+        longer_kmers=[]
+        for mer in shorter_kmers:
+            longer_kmers.append(mer+'A')
+            longer_kmers.append(mer+'C')
+            longer_kmers.append(mer+'G')
+            longer_kmers.append(mer+'T')
+        shorter_kmers = longer_kmers
+    all_kmers = shorter_kmers
+    kmer_dict = {}
+    kmer_dict[npad]=0
+    value=1
+    for mer in all_kmers:
+        kmer_dict[mer]=value
+        value += 1
+    return kmer_dict
