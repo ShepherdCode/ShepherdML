28a58b1ea3478dbef0fb1d31d74276829dd9f846
JasonRafeMiller <jmill02@shepherd.edu>
2021-01-22 19:24:11 -0500

    Achieved 80% accuracy on medians.

---
+++ Nasa2021/tools_fasta.py
@@ -0,0 +1,150 @@
+'''
+TO DO: make this a proper python module.
+HOW TO USE:
+import tools_fasta as tools
+tools.yahoo()
+'''
+
+# TO DO: write tests / demos
+# TO DO: remove pandas dependency just to open FASTA?
+# TO DO: stop using K=1 and NNN=0
+
+import pandas as pd
+import numpy as np
+
+def yahoo():
+    '''Run this to verify the code import.'''
+    print("Yahoo!")
+
+# Assume file was preprocessed to contain one line per seq.
+# Prefer Pandas dataframe but df does not support append.
+# For conversion to tensor, must avoid python lists.
+def load_fasta(filename,label):
+    DEFLINE='>'
+    labels=[]
+    seqs=[]
+    lens=[]
+    nums=[]
+    num=0
+    with open (filename,'r') as infile:
+        for line in infile:
+            if line[0]!=DEFLINE:
+                seq=line.rstrip()
+                num += 1   # first seqnum is 1
+                seqlen=len(seq)
+                nums.append(num)
+                labels.append(label)
+                seqs.append(seq)
+                lens.append(seqlen)
+    df1=pd.DataFrame(nums,columns=['seqnum'])
+    df2=pd.DataFrame(labels,columns=['class'])
+    df3=pd.DataFrame(seqs,columns=['sequence'])
+    df4=pd.DataFrame(lens,columns=['seqlen'])
+    df=pd.concat((df1,df2,df3,df4),axis=1)
+    return df
+
+def make_kmer_table(K):
+    npad='N'*K
+    shorter_kmers=['']
+    for i in range(K):
+        longer_kmers=[]
+        for mer in shorter_kmers:
+            longer_kmers.append(mer+'A')
+            longer_kmers.append(mer+'C')
+            longer_kmers.append(mer+'G')
+            longer_kmers.append(mer+'T')
+        shorter_kmers = longer_kmers
+    all_kmers = shorter_kmers
+    kmer_dict = {}
+    kmer_dict[npad]=0
+    value=1
+    for mer in all_kmers:
+        kmer_dict[mer]=value
+        value += 1
+    return kmer_dict
+
+def separate_X_and_y(data):
+    y=   data[['class']].copy()
+    X=   data.drop(columns=['class','seqnum','seqlen'])
+    return (X,y)
+
+def strings_to_vectors(data,uniform_len,K):
+    all_seqs=[]
+    KMER_TABLE=make_kmer_table(K)
+    for seq in data['sequence']:
+        i=0
+        seqlen=len(seq)
+        kmers=[]
+        while i < seqlen-K+1 -1:
+            kmer=seq[i:i+K]
+            i += 1
+            value=KMER_TABLE[kmer]
+            kmers.append(value)
+        pad_val=0
+        while i < uniform_len:
+            kmers.append(pad_val)
+            i += 1
+        all_seqs.append(kmers)
+    pd2d=pd.DataFrame(all_seqs)
+    return pd2d   # return 2D dataframe, uniform dimensions
+
+def make_kmers(K,MAXLEN,train_set):
+    # TO DO: move data separation out of this function
+    (X_train_all,y_train_all)=separate_X_and_y(train_set)
+    X_train_kmers=strings_to_vectors(X_train_all,MAXLEN,K)
+    # From pandas dataframe to numpy to list to numpy
+    num_seqs=len(X_train_kmers)
+    tmp_seqs=[]
+    for i in range(num_seqs):
+        kmer_sequence=X_train_kmers.iloc[i]
+        tmp_seqs.append(kmer_sequence)
+    X_train_kmers=np.array(tmp_seqs)
+    tmp_seqs=None
+    labels=y_train_all.to_numpy()
+    return (X_train_kmers,labels)
+
+def make_frequencies(K,Xin):
+    Xout=[]
+    VOCABULARY_SIZE= 4**K + 1  # plus one for 'NNN'
+    for seq in Xin:
+        freqs =[0] * VOCABULARY_SIZE
+        total = 0
+        for kmerval in seq:
+            freqs[kmerval] += 1
+            total += 1
+        for c in range(VOCABULARY_SIZE):
+            freqs[c] = freqs[c]/total
+        Xout.append(freqs)
+    Xnum = np.asarray(Xout)
+    return (Xnum)
+
+def make_slice(data_set,min_len,max_len):
+    '''data_set: pandas data frame with seqlen column'''
+    slice = data_set.query('seqlen <= '+str(max_len)+' & seqlen>= '+str(min_len))
+    return slice
+
+def onehot(seqs):
+    '''Input list of digits from alphabet {0..4}.
+    Return array of (5-bit bit-vectors).'''
+    newX = []
+    vectors=[]
+    vectors.append([1,0,0,0,0]) # 0 = N
+    vectors.append([0,1,0,0,0]) # 1 = A
+    vectors.append([0,0,1,0,0]) # 2 = C
+    vectors.append([0,0,0,1,0]) # 3 = G
+    vectors.append([0,0,0,0,1]) # 4 = T
+    for seq in seqs:
+        letters=[]
+        for num in seq:
+            hot = vectors[num]
+            letters.append(hot)
+        newX.append(letters)
+    return np.asarray(newX)
+
+ONEHOT5=dict(zip('ACGTN',[[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]]))
+def one_hot (str):
+    return [ONEHOT5[x] for x in str]
+def uniform_length(lst,UNI_LEN):
+    # For speed, assume no element longer than UNI_LEN.
+    # This function breaks if assumption is violated.
+    return np.array([i + 'N'*(UNI_LEN-len(i)) for i in lst])
