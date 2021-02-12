13a495db972ae8814f3b3d0bf59f65ad86eb8ad9
JasonRafeMiller <jmill02@shepherd.edu>
2021-02-11 19:49:12 -0500

    Fix bug that added one space to each RNA.

--- Nasa2021/make_balanced_sets.sh
+++ Nasa2021/make_balanced_sets.sh
@@ -15,7 +15,7 @@ echo
 
 echo "How many sequences finally?"
 cat ncRNA.tmp | \
-    awk '{if(substr($1,1,1)==">") x=$1; else {print x, "&", $1;}}' \
+    awk '{if(substr($1,1,1)==">") x=$1; else {print x "&" $1;}}' \
 	> concat.tmp
 shuf concat.tmp | head -n $total > random.tmp
 cat random.tmp | tr '&' '\n' > ncRNA.gc36.balance.fasta
@@ -23,4 +23,4 @@ grep -c '^>' *.fasta
 echo
 
 echo "Cleanup..."
-rm -v *.tmp
+#rm -v *.tmp
