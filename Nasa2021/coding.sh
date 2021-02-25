5cc311aca57b326fffd1b63a7184f0e632dc7881
John Little <jalittle476@gmail.com>
2021-02-25 17:06:53 -0500

    updated coding and noncoding scripts

--- Nasa2021/coding.sh
+++ Nasa2021/coding.sh
@@ -1,48 +1,49 @@
-python GenFASTA.py 1000 True 1 coding_1000_1.fasta 
-python GenFASTA.py 1000 True 2 coding_1000_2.fasta 
-python GenFASTA.py 1000 True 3 coding_1000_3.fasta 
+#!/bin/bash
+python3 GenFASTA.py 1000 True 1 coding_1000_1.fasta 
+python3 GenFASTA.py 1000 True 2 coding_1000_2.fasta 
+python3 GenFASTA.py 1000 True 3 coding_1000_3.fasta 
 cat coding_1000_?.fasta > coding_3000.fasta 
 rm coding_1000_1.fasta 
 rm coding_1000_2.fasta 
 rm coding_1000_3.fasta 
-python GenFASTA.py 2000 True 1 coding_2000_1.fasta 
-python GenFASTA.py 2000 True 2 coding_2000_2.fasta 
-python GenFASTA.py 2000 True 3 coding_2000_3.fasta 
+python3 GenFASTA.py 2000 True 1 coding_2000_1.fasta 
+python3 GenFASTA.py 2000 True 2 coding_2000_2.fasta 
+python3 GenFASTA.py 2000 True 3 coding_2000_3.fasta 
 cat coding_2000_?.fasta > coding_6000.fasta 
 rm coding_2000_1.fasta 
 rm coding_2000_2.fasta 
 rm coding_2000_3.fasta 
-python GenFASTA.py 4000 True 1 coding_4000_1.fasta 
-python GenFASTA.py 4000 True 2 coding_4000_2.fasta 
-python GenFASTA.py 4000 True 3 coding_4000_3.fasta 
+python3 GenFASTA.py 4000 True 1 coding_4000_1.fasta 
+python3 GenFASTA.py 4000 True 2 coding_4000_2.fasta 
+python3 GenFASTA.py 4000 True 3 coding_4000_3.fasta 
 cat coding_4000_?.fasta > coding_12000.fasta 
 rm coding_4000_1.fasta 
 rm coding_4000_2.fasta 
 rm coding_4000_3.fasta 
-python GenFASTA.py 8000 True 1 coding_8000_1.fasta 
-python GenFASTA.py 8000 True 2 coding_8000_2.fasta 
-python GenFASTA.py 8000 True 3 coding_8000_3.fasta 
+python3 GenFASTA.py 8000 True 1 coding_8000_1.fasta 
+python3 GenFASTA.py 8000 True 2 coding_8000_2.fasta 
+python3 GenFASTA.py 8000 True 3 coding_8000_3.fasta 
 cat coding_8000_?.fasta > coding_24000.fasta 
 rm coding_8000_1.fasta 
 rm coding_8000_2.fasta 
 rm coding_8000_3.fasta 
-python GenFASTA.py 16000 True 1 coding_16000_1.fasta 
-python GenFASTA.py 16000 True 2 coding_16000_2.fasta 
-python GenFASTA.py 16000 True 3 coding_16000_3.fasta 
+python3 GenFASTA.py 16000 True 1 coding_16000_1.fasta 
+python3 GenFASTA.py 16000 True 2 coding_16000_2.fasta 
+python3 GenFASTA.py 16000 True 3 coding_16000_3.fasta 
 cat coding_16000_?.fasta > coding_48000.fasta 
 rm coding_16000_1.fasta 
 rm coding_16000_2.fasta 
 rm coding_16000_3.fasta 
-python GenFASTA.py 32000 True 1 coding_32000_1.fasta 
-python GenFASTA.py 32000 True 2 coding_32000_2.fasta 
-python GenFASTA.py 32000 True 3 coding_32000_3.fasta 
+python3 GenFASTA.py 32000 True 1 coding_32000_1.fasta 
+python3 GenFASTA.py 32000 True 2 coding_32000_2.fasta 
+python3 GenFASTA.py 32000 True 3 coding_32000_3.fasta 
 cat coding_32000_?.fasta > coding_96000.fasta 
 rm coding_32000_1.fasta 
 rm coding_32000_2.fasta 
 rm coding_32000_3.fasta 
-python GenFASTA.py 64000 True 1 coding_64000_1.fasta 
-python GenFASTA.py 64000 True 2 coding_64000_2.fasta 
-python GenFASTA.py 64000 True 3 coding_64000_3.fasta 
+python3 GenFASTA.py 64000 True 1 coding_64000_1.fasta 
+python3 GenFASTA.py 64000 True 2 coding_64000_2.fasta 
+python3 GenFASTA.py 64000 True 3 coding_64000_3.fasta 
 cat coding_64000_?.fasta > coding_192000.fasta 
 rm coding_64000_1.fasta 
 rm coding_64000_2.fasta 
--- Nasa2021/noncoding.sh
+++ Nasa2021/noncoding.sh
@@ -1,7 +1,7 @@
-python GenFASTA.py 3000 False 3 noncod_3000.fasta 
-python GenFASTA.py 6000 False 3 noncod_6000.fasta 
-python GenFASTA.py 12000 False 3 noncod_12000.fasta 
-python GenFASTA.py 24000 False 3 noncod_24000.fasta 
-python GenFASTA.py 48000 False 3 noncod_48000.fasta 
-python GenFASTA.py 96000 False 3 noncod_96000.fasta 
-python GenFASTA.py 192000 False 3 noncod_192000.fasta 
+python3 GenFASTA.py 3000 False 3 noncod_3000.fasta 
+python3 GenFASTA.py 6000 False 3 noncod_6000.fasta 
+python3 GenFASTA.py 12000 False 3 noncod_12000.fasta 
+python3 GenFASTA.py 24000 False 3 noncod_24000.fasta 
+python3 GenFASTA.py 48000 False 3 noncod_48000.fasta 
+python3 GenFASTA.py 96000 False 3 noncod_96000.fasta 
+python3 GenFASTA.py 192000 False 3 noncod_192000.fasta 
