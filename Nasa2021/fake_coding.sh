ef63f44dd04f7430b7f725f533ab840dab89e847
Keagen <49502992+KeagenT@users.noreply.github.com>
2021-02-25 15:25:25 -0500

    scripts to generate fasta files for the experiment

---
+++ Nasa2021/fake_coding.sh
@@ -0,0 +1,7 @@
+python GenFASTA.py 3000 False 3 fake_coding_3000.fasta 
+python GenFASTA.py 6000 False 3 fake_coding_6000.fasta 
+python GenFASTA.py 12000 False 3 fake_coding_12000.fasta 
+python GenFASTA.py 24000 False 3 fake_coding_24000.fasta 
+python GenFASTA.py 48000 False 3 fake_coding_48000.fasta 
+python GenFASTA.py 96000 False 3 fake_coding_96000.fasta 
+python GenFASTA.py 192000 False 3 fake_coding_192000.fasta 
