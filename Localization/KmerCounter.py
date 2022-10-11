6ee922069c62fd552ec02923a0b9deec7307d083
JasonRafeMiller <jmill02@shepherd.edu>
2022-10-11 17:48:03 -0400

    Redo the TrainTest split.

--- Localization/KmerCounter.py
+++ Localization/KmerCounter.py
@@ -1,13 +1,14 @@
 import numpy as np
+import regex
+
 class KmerCounter():
     NUCLEOTIDE_BITS={'A':0, 'C':1, 'G':2, 'T':3}
     TOTAL_NUCLEOTIDES=4
     BITS_PER_NUCLEOTIDE=2
-    COUNT_TYPE=np.int8
+    COUNT_TYPE=np.uint8
     MAX_COUNT = 2**8-1
     def __init__(self,K=4):
         self.setK(K)
-        self.OPTIMIZE=False
     def setK(self,K):
         self.K=K
         self.VOCABULARY_SIZE = KmerCounter.TOTAL_NUCLEOTIDES**K
@@ -18,8 +19,6 @@ class KmerCounter():
         self.MASK=int(mask,2)
     def get_vocabulary_size(self):
         return self.VOCABULARY_SIZE
-    def optimize(self):
-        self.OPTIMIZE = True
     def hash_value(self,token):
         '''
         Get the counts array index for the given K-mer.
@@ -41,12 +40,15 @@ class KmerCounter():
                 return None
         return kmer_hash
     def subsequent_value(self,next_letter,prev_value):
+        '''
+        An optimization used by count_by_letter().
+        '''
         bits = KmerCounter.NUCLEOTIDE_BITS[next_letter]  # protected against N
         masked = prev_value & self.MASK   # erase 2 left-most bits of hash_value
         shifted = masked << 2             # make room for 2 bits
         next_value = shifted + bits       # add 2 new bits at right of hash_value
         return next_value
-    def seq_to_kmer_counts(self,seq):
+    def seq_to_kmer_counts(self,seq,method=3):
         '''
         Clients should call setK() once,
         then seq_to_kmer_counts for each DNA sequence.
@@ -54,6 +56,48 @@ class KmerCounter():
         so maximum count is 127.
         For example, after setK(4) and counts=seq_to_kmer_counts('AAAAA'),
         then counts(hash_value('AAAA')) will be 2.
+        The default method is recommended.
+        '''
+        if method==1:
+            return self.count_using_regex(seq)
+        elif method==2:
+            return self.count_by_letter(seq)
+        else:   # 3 default
+            return self.count_by_token(seq)
+    def count_using_regex(self,seq):
+        '''
+        This is a reference implementation.
+        '''
+        counts = np.zeros(
+            self.VOCABULARY_SIZE, KmerCounter.COUNT_TYPE)
+        token = 'A'*self.K
+        i=0
+        while token is not None:
+            # string.count(substr) does not count overlapping substrings!
+            # counts[i] = seq.count(token)
+            actual = len(regex.findall(token, seq, overlapped=True))
+            capped = min(actual,KmerCounter.MAX_COUNT)
+            counts[i] = capped
+            i += 1
+            token=self.next_kmer(token)
+        return counts
+    def count_by_token(self,seq):
+        counts = np.zeros(
+            self.VOCABULARY_SIZE, KmerCounter.COUNT_TYPE)
+        for p in range(len(seq)-self.K+1):
+            next_token=seq[p:p+self.K]
+            hash_value = self.hash_value(next_token)
+            if hash_value is not None:
+                if counts[hash_value]<KmerCounter.MAX_COUNT:
+                    counts[hash_value] += 1
+        return counts
+    def count_by_letter(self,seq):
+        '''
+        Optimized way to count K-mers in DNA.
+        When hashing a K-mer, re-use the bits from the last K-1 letters.
+        Given 'CCCCT', after hashing 'CCCC', just add the bit-value of T.
+        On first K-mer, and on K-mers with N, must revert to full count.
+        In tests on mRNA, this was no faster than count_by_token.
         '''
         prev_value = None
         next_letter = None
@@ -61,8 +105,7 @@ class KmerCounter():
             self.VOCABULARY_SIZE, KmerCounter.COUNT_TYPE)
         for p in range(len(seq)-self.K+1):
             next_letter = seq[p+self.K-1]
-            if self.OPTIMIZE and \
-            prev_value is not None and \
+            if prev_value is not None and \
             next_letter in KmerCounter.NUCLEOTIDE_BITS:
                 hash_value = self.subsequent_value(next_letter,prev_value)
                 prev_value = hash_value
@@ -74,3 +117,29 @@ class KmerCounter():
                 if counts[hash_value]<KmerCounter.MAX_COUNT:
                     counts[hash_value] += 1
         return counts
+    def next_kmer(self,token)->str:
+        '''
+        Designed to be called in a feed-forward loop.
+        Example: next_kmer('AAAA') returns 'AAAC'.
+        Example: next_kmer('ATTT') returns 'CAAA'.
+        Example: next_kmer('TTTT') returns None.
+        '''
+        p = len(token)-1
+        next_token = list(token)
+        while p>=0:
+            if token[p]=='A':
+                next_token[p]='C'
+                word=''.join(next_token)
+                return word
+            if token[p]=='C':
+                next_token[p]='G'
+                word=''.join(next_token)
+                return word
+            if token[p]=='G':
+                next_token[p]='T'
+                word=''.join(next_token)
+                return word
+            if token[p]=='T':
+                next_token[p]='A'
+                p = p-1
+        return None
--- Localization/TrainValidSplit.py
+++ Localization/TrainValidSplit.py
@@ -1,5 +1,8 @@
 from random import Random
+from csv import reader
 import numpy as np
+import math
+
 class Splitter():
     def __init__(self):
         self.gid_tid = []
@@ -17,6 +20,52 @@ class Splitter():
         generator.seed(42)
         self.gene_list = list(gene_set)
         generator.shuffle(self.gene_list)  # in-place
+    def load_sequences(self,gencode_file,labeled_genes):
+        gid_tid = []
+        with open(gencode_file,'r') as gencode:
+            header = None
+            csv = reader(gencode)
+            rownum = 0
+            for row in csv:
+                if header is None:
+                    header = row
+                else:
+                    tran_id = row[0]
+                    gene_id = row[1]
+                    if gene_id in labeled_genes:
+                        label = labeled_genes[gene_id]
+                        gid_tid.append((gene_id,tran_id,rownum,label))
+                rownum += 1
+        return gid_tid
+    def load_labels(self,atlas_file,cells,RCI_THRESHOLDS):
+        gene_labels = {}
+        with open(atlas_file,'r') as atlas:
+            header = None
+            genes_considered = 0
+            positives = 0
+            csv = reader(atlas)
+            for row in csv:
+                if header is None:
+                    header = row
+                else:
+                    gene = row[0]
+                    genes_considered += 1
+                    rci = float(row[1+cells])
+                    if not math.isnan(rci):
+                        # GENERATE BINARY LABELS
+                        if rci < RCI_THRESHOLDS[0]:
+                            label = 0   # RCI<0
+                            gene_labels[gene]=label
+                        elif rci >= RCI_THRESHOLDS[1]:
+                            label = 1   #RCI>=0
+                            positives += 1
+                            gene_labels[gene]=label
+        # TO DO: make this optional or not necessary
+        print('ATLAS Genes:',genes_considered)
+        print('Filter for genes with RCI labels from cell line:',cells)
+        print('Labeled genes:',len(gene_labels))
+        print('Positive labels:',positives)
+        return gene_labels
     def train_valid_split(self,iteration, partitions):
         # TO DO: make sure we don't leave out the last sequence due to rounding
         partition_size = int(len(self.gene_list) * 1.0/partitions)
