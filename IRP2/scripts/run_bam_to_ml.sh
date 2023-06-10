ba1333959db35a373bbe121996fac93d707f7f58
JasonRafeMiller <jmill02@shepherd.edu>
2023-06-10 15:50:10 -0400

    We now extact quality features. This is running on Saga.

--- IRP2/scripts/run_bam_to_ml.sh
+++ IRP2/scripts/run_bam_to_ml.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+echo "If necessary, load the samtools module."
+echo "If necessary, load the python module."
+
+SRC="/cluster/projects/nn9525k/hybrids/jasonrm/Arenosa/scripts"
+
+date
+echo "Processing ..."
+samtools view Primary.bam | python ${SRC}/bam_to_ml_csv.py > ml_stats.csv
+echo -n $?
+echo " exit status"
+
 date
 echo "Total read pairs in bam:"
 samtools view Primary.bam | cut -f 1 | uniq | wc -l
-
-date
-echo "processing ..."
-samtools view Primary.bam | python bam_to_ml_csv.py > ml_stats.csv
-echo -n $?
-echo " exit status"
 
 date
 echo "Total read pairs in cvs:"
