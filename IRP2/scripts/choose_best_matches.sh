f532142daefef551d14855c536f4bc90dda5d9f7
JasonRafeMiller <jmill02@shepherd.edu>
2023-06-19 09:36:27 -0400

    Scripts applied to halleri+lyrata transcripts.

---
+++ IRP2/scripts/choose_best_matches.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+echo Download cDNA transcriptomes
+
+# wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/current/fasta/arabidopsis_lyrata/cdna/Arabidopsis_lyrata.v.1.0.cdna.all.fa.gz
+# wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/current/fasta/arabidopsis_halleri/cdna/Arabidopsis_halleri.Ahal2.2.cdna.all.fa.gz
+# gunzip *.fa.gz
+
+echo Align transcripts
+
+# nucmer Arabidopsis_halleri.Ahal2.2.cdna.all.fa Arabidopsis_lyrata.v.1.0.cdna.all.fa
+# mv out.delta halleri_lyrata.delta
+
+echo Select allele pairs 
+
+# Use show-coords from nucmer/mummer.
+# Reduce the alignments to at most one per pair of alleles.
+# Insist the alignment covers 75% of both transcripts.
+# Sort the halleri length reverse, and keep the longest alignment per halleri id.
+# Then do the same for lyrata.
+# The result is 15093 pairs of IDs and each ID occurs once at most.
+./show-coords -H -T -c halleri_lyrata.delta \
+    | grep '\.t1' \
+    | awk '{if ($7>75 && $8>75) print $0;}' \
+    | awk '{printf "%10s\t%6d\t%s\n", $10, $5, $11;}' \
+    | sort -r \
+    | awk '{if ($1!=p) printf "%30s\t%6d\t%s\n", $3, $2, $1; p=$1;}' \
+    | sort -r \
+    | awk '{if ($1!=p) print $0; p=$1;}' \
+    > best_matches.lyrata_len_halleri.txt
+
+echo Create fasta files formatted for IRP scripts
+
+python make_fasta.py
+
+echo Create the synthetic diploid
+
+cat halleri.fasta lyrata.fasta > diploid.fasta
+
+echo Compress files
+
+gzip -v *.fa
+gzip -v lyrata.fasta halleri.fasta
+
+echo Done
---
+++ IRP2/scripts/make_fasta.py
@@ -0,0 +1,79 @@
+'''
+Write two fasta files.
+One for lyrata, one for halleri.
+Both have the same number of sequences.
+Each sequence in one has exactly one match in the other.
+Both IDs are recorded in each defline.
+For ease of processing, remove internal newlines from sequences.
+'''
+
+map_L_to_H = dict()
+map_H_to_L = dict()
+match_file = 'best_matches.lyrata_len_halleri.txt'
+
+def load_matches(filename):
+    with open (filename,'r') as fin:
+        for line in fin:
+            line=line.strip()
+            fields=line.split('\t')
+            L = fields[0]
+            length = fields[1]
+            H = fields[2]
+            map_L_to_H[L]=H
+            map_H_to_L[H]=L
+
+load_matches(match_file)
+
+#print('Just testing:')
+#test_L = 'scaffold_81700001.1'
+#print(test_L, 'maps to', map_L_to_H[test_L])
+#test_H = 'g10924.t1'
+#print(test_H, 'maps to', map_H_to_L[test_H])
+
+def write_fasta(infilename,outfilename,allele):
+    with open (infilename,'r') as fin:
+        with open (outfilename,'w') as fout:
+            first_line = True
+            defline = None
+            for line in fin:
+                line=line.strip()
+                if line.startswith('>'):
+                    if defline is not None:
+                        if first_line:
+                            first_line=False
+                        else:
+                            fout.write('\n') # end prev sequence
+                    defline = None
+                    tid = line.split(' ')[0][1:]
+                    if allele=='halleri':
+                        if tid in map_H_to_L.keys():
+                            # Write tid of best match in lyrata just for tracking
+                            hid = tid
+                            lid = map_H_to_L[tid]
+                            defline = '>'+hid+'_halleri '+lid+'\n'
+                    else:
+                        if tid in map_L_to_H.keys():
+                            # Use tid of best match in halleri to emphasize same gene
+                            # Write original lyrata tid just for tacking 
+                            hid = map_L_to_H[tid]
+                            defline = '>'+hid+'_lyrata '+tid+'\n'
+                    if defline is not None:
+                        # We intentionally remove newlines from sequence 
+                        fout.write(defline) # start next sequence
+                else:
+                    if defline is not None:
+                        fout.write(line)  # more sequence
+            # end last sequence
+            if defline is not None:            
+                fout.write('\n')
+                defline = None
+
+print('Writing lyrata...')
+lyrata_in = 'Arabidopsis_lyrata.v.1.0.cdna.all.fa'
+lyrata_out = 'lyrata.fasta'
+write_fasta(lyrata_in,lyrata_out,'lyrata')
+print('Writing halleri...')
+halleri_in = 'Arabidopsis_halleri.Ahal2.2.cdna.all.fa'
+halleri_out = 'halleri.fasta'
+write_fasta(halleri_in,halleri_out,'halleri')
+print('Wrote two fasta files.')
